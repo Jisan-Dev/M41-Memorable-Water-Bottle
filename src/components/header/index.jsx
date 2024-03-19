90a4c0769261d16e481a443ebe84e976573a3699
Jisun696 <istiakkashem35@gmail.com>
2024-03-19 23:55:37 +0600

    added a responsive header and nav

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,32 @@
+import { useState } from 'react';
+import './header.css';
+import { RiMenu3Line } from 'react-icons/ri';
 const Header = () => {
+  const [cartClicked, setCartClicked] = useState(false);
+  const routes = [
+    { id: 1, path: '/', name: 'Home' },
+    { id: 2, path: '/about', name: 'About' },
+    { id: 3, path: '/cart', name: 'Cart' },
+  ];
+
   return (
-    <div>
-      <h1>Memorable Water Bottle</h1>
-    </div>
+    <>
+      <nav>
+        <RiMenu3Line onClick={() => setCartClicked(!cartClicked)} className="nav-icon" />
+        {
+          <ul style={{ top: cartClicked ? '40px' : '-200px' }} className="nav-ul">
+            {routes.map((route) => (
+              <li key={route.id}>
+                <a href={route.path}>{route.name}</a>
+              </li>
+            ))}
+          </ul>
+        }
+      </nav>
+      <div>
+        <h1>Memorable Water Bottle</h1>
+      </div>
+    </>
   );
 };
 
